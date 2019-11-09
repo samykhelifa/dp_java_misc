c1e1bc46cf682e5c87beed6919fe727871494f35
samy <39716963+samykhelifa@users.noreply.github.com>
2019-11-09 05:08:07 +0100

    Create Duck.java

---
+++ creational/abstract_factory/baeldung/Duck.java
@@ -0,0 +1,12 @@
+public class Duck implements Animal {
+ 
+    @Override
+    public String getAnimal() {
+        return "Duck";
+    }
+ 
+    @Override
+    public String makeSound() {
+        return "Squeks";
+    }
+}
