c76add19326f185b0038a3c30a5c950956d0fde3
samy <39716963+samykhelifa@users.noreply.github.com>
2019-11-09 05:15:34 +0100

    Create Dog.java

---
+++ creational/abstract_factory/baeldung/Dog.java
@@ -0,0 +1,13 @@
+public class Dog implements Animal {
+
+    @Override
+    public String getType() {
+        return "Dog";
+    }
+
+    @Override
+    public String makeSound() {
+        return "Barks";
+    }
+
+}
